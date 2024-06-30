485fc8bbec97afce5a138beaba18b3b47c35ba91
namestesensei <christopherwright435@gmail.com>
2024-06-30 23:34:55 +0000

    main.h1.0

--- main.h
+++ main.h
@@ -1,1 +1,64 @@
-hello do you see me.
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Standard Libraries */
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/**
+ * _printf - Produces output according to a format.
+ * @format: A character string. The format string is composed of zero or more
+ * directives. See man 3 printf for more detail.
+ *
+ * Return: The number of characters printed (excluding the null byte
+ * used to end output to strings).
+ */
+int _printf(const char *format, ...);
+
+/**
+ * _putchar - Writes a character to stdout.
+ * @c: The character to print.
+ *
+ * Return: On success, returns the number of characters printed (1).
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * print_char - Prints a character.
+ * @args: A list of variadic arguments.
+ *
+ * Return: The number of characters printed (1).
+ */
+int print_char(va_list args);
+
+/**
+ * print_string - Prints a string.
+ * @args: A list of variadic arguments.
+ *
+ * Return: The number of characters printed.
+ */
+int print_string(va_list args);
+
+/**
+ * print_int - Prints an integer.
+ * @args: A list of variadic arguments.
+ *
+ * Return: The number of characters printed.
+ */
+int print_int(va_list args);
+
+/**
+ * get_function - Selects the correct function to perform the operation
+ * requested by the format specifier.
+ * @s: The format specifier passed to _printf.
+ *
+ * Return: A pointer to the function that corresponds to the format specifier.
+ */
+int (*get_function(char s))(va_list args);
+
+#endif /* MAIN_H */
+
