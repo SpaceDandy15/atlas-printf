9e8df9ab317edb6e54e52486954e73c880004c32
Malik Vance <malik@atlassian.com>
2024-06-30 19:43:43 -0500

    i cleaned up main h. Then moved main h file defs to manual for help later.

--- main.h
+++ main.h
@@ -6,64 +6,28 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+
+/**
+ * struc func_type - type structure
+ * @p: pointer to the argument
+ * @f: pointer-function associated with the argument
+*/
+typedef struct func_type
+{
+	char *p;
+	int (*f)(va_list);
+}func_t;
 
 /* Function Prototypes */
-
-/**
- * _printf - Produces output according to a format.
- * @format: A character string. The format string is composed of zero or more
- * directives. See man 3 printf for more detail.
- *
- * Return: The number of characters printed (excluding the null byte
- * used to end output to strings).
- */
 int _printf(const char *format, ...);
-
-/**
- * _putchar - Writes a character to stdout.
- * @c: The character to print.
- *
- * Return: On success, returns the number of characters printed (1).
- * On error, -1 is returned, and errno is set appropriately.
- */
 int _putchar(char c);
-
-/**
- * print_char - Prints a character.
- * @args: A list of variadic arguments.
- *
- * Return: The number of characters printed (1).
- */
+int print_dec(va_list args);
 int print_char(va_list args);
+int print_str(va_list args);
+int (*get_funct(char s))(va_list args);
+int print_pct(va_list args);
 
-/**
- * print_string - Prints a string.
- * @args: A list of variadic arguments.
- *
- * Return: The number of characters printed.
- */
-int print_string(va_list args);
-
-/**
- * print_int - Prints an integer.
- * @args: A list of variadic arguments.
- *
- * Return: The number of characters printed.
- */
-int print_int(va_list args);
-
-/**
- * get_function - Selects the correct function to perform the operation
- * requested by the format specifier.
- * @s: The format specifier passed to _printf.
- *
- * Return: A pointer to the function that corresponds to the format specifier.
- */
-int (*get_function(char s))(va_list args);
-
-#endif /* MAIN_H */
-
-=======
-hello do you see me.
-i do can you see me?
->>>>>>> d7871f5b73c137d8887ee83096a7ed8e459b104b
+#endif
