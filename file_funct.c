142864b42e0cc80c401de49fa067031effc8c1ee
Malik Vance <malik@atlassian.com>
2024-06-30 23:55:11 -0500

    different print f approach and filefunc

--- file_funct.c
+++ file_funct.c
@@ -1,61 +1,47 @@
 #include "main.h"
-#include <unistd.h> /* Include for write function */
 
 /**
- * print_char - Print a single character
- * @args: va_list containing the character to print
- * Return: Number of characters printed
- */
+ * print_char - prints character 
+ * @args: argument
+ *
+ * Return: character
+*/
+
 int print_char(va_list args)
 {
-    char c = va_arg(args, int);
-    return write(1, &c, 1);
+	_putchar(va_arg(args, int));
+	return (1);
 }
 
 /**
- * print_str - Print a string of characters
- * @args: va_list containing the string to print
- * Return: Number of characters printed
- */
+ * print_str - prints string
+ * @args: arguments
+ * Return: string
+*/
+
 int print_str(va_list args)
 {
-    char *str = va_arg(args, char *);
-    int len = 0;
-    if (!str)
-        str = "(null)";
-    while (str[len])
-        len++;
-    return write(1, str, len);
+	int m;
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+	str = "(null)";
+	for (m = 0; str[m]; m++);
+	{
+		_putchar(str[m]);
+	}
+	return (m);
 }
 
 /**
- * print_pct - Print a percent sign '%'
- * @args: va_list (unused)
- * Return: Always returns 1 (the number of characters printed)
- */
+ * print_pct - prints character "%"
+ * @args : arguments 
+ *
+ * Return: character "%"
+*/
+
 int print_pct(va_list args)
 {
-    (void)args;
-    return write(1, "%", 1);
+	(void)args;
+	return (write(1, "%", 1));
 }
-
-/**
- * print_dec - Print a decimal number
- * @args: va_list containing the integer to print
- * Return: Number of characters printed
- */
-int print_dec(va_list args)
-{
-    int num = va_arg(args, int);
-    int count = 0;
-    if (num < 0)
-    {
-        count += write(1, "-", 1);
-        num = -num;
-    }
-    if (num / 10)
-        count += print_dec(num / 10);
-    count += write(1, &"0123456789"[num % 10], 1);
-    return count;
-}
-
--- printf.c
+++ printf.c
@@ -1,45 +1,51 @@
 #include "main.h"
+#include <stdarg.h>
 
 /**
- * _printf - Custom printf function implementation
- * @format: Format string
- * Return: Number of characters printed (excluding null byte)
- */
-int _printf(const char *format, ...)
+ * _pritnf - pritns to output according to format
+ * @format: character string
+ *
+ * Return: number of characetrs printed
+*/
+
+int _pritnf(conts char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    func_t p[] = {
-        {'c', print_char},
-        {'s', print_str},
-        {'%', print_pct},
-        {'d', print_dec},
-        {'i', print_dec},
-        {0, NULL}
-    };
+	int i = 0, m = 0;
+	int (*f) (va_list);
+	va_list args;
 
-    va_start(args, format);
-
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            for (int i = 0; p[i].t; i++)
-            {
-                if (*format == p[i].t)
-                    count += p[i].f(args);
-            }
-        }
-        else
-        {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
-
-    va_end(args);
-    return count;
+	va_start(args, format);
+	if (format == NULL || !format[i + 1])
+	return (-1);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1])
+			{
+				if (format[i + 1] != 'c' && format[i +1] != 's'
+				&& format[i + 1] != '%' && fomrat[i + 1] != 'd'
+				&& format[ i + 1] != 'i')
+				{
+					m += _putchar(format[i]);
+					m += _putchar(format[i+ 1]);
+					i++;
+				}
+				else
+				{
+					f = get_func(&format[i +1]);
+					m += f(args);
+					i++;
+				}
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			m++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (m);
 }
-
