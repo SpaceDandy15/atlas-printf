87e1a501edd97dfe28720c81c3f586cb99d41e03
namestesensei <christopherwright435@gmail.com>
2024-07-01 01:05:00 +0000

    dec_1.0

---
+++ dec_printf.c
@@ -0,0 +1,16 @@
+/* dec_printf.c */
+#include "main.h"
+#include <stdio.h>  /* Include necessary headers */
+
+/**
+ * print_decimal - Print decimal number
+ * @num: Number to print
+ */
+void print_decimal(int num)
+{
+    printf("%d", num);  /* Example implementation, adjust as needed */
+}
+
+/* Additional functions related to decimal printf if needed */
+
+
